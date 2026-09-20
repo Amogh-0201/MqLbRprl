4adf79518343eafaa7ee63881340199b002eb4c5
Amogh-0201 <dasbarmanamogh@gmail.com>
2026-09-20 14:05:28 +0530

    implemented basic load balancer with 3 server instances from scratch without using any tool/framework

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 
-//middlewares
+// middlewares
 const errorHandlerMiddleware = require("./middlewares/errorhandler_middleware");
 const notFound = require("./middlewares/not_found");
 const connectDb = require("./db/connectDb");
 
-//routes
+// routes
 const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
 const orderRouter = require("./routes/orderRoute")
@@ -15,15 +15,38 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+// instance ids for different server instances
+const instanceId = process.env.INSTANCE_ID || `instance-${process.pid}`;
+
+// Basic Middleware
 app.use(express.json());
 
+// to show load balancing is actually happening
+app.use((req, res, next) => {
+    res.setHeader("X-INSTANCE-ID", instanceId);
+    next();
+});
+
+// health check
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        status: "ok",
+        instanceId,
+        pid: process.pid
+    });
+});
+
+// all routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/orders", orderRouter);
 
+// Not Found and Error Handler Middlewares
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 
+
+// Server Startup
 async function start() {
     await connectDb();
     app.listen(port, () => {
---
+++ load-balancer.js
@@ -0,0 +1,216 @@
+require("dotenv").config();
+const http = require("node:http");
+
+const LB_PORT = process.env.LB_PORT || 4000;
+
+const HEALTH_CHECK_INTERVAL = 5000;
+const HEALTH_CHECK_TIMEOUT = 1500;
+const REQUEST_TIMEOUT = 5000;
+
+// Backend Replicas
+const serverReplicas = [
+    {
+        id: "server-1",
+        host: "127.0.0.1",
+        port: 3001,
+        healthy: false
+    },
+    {
+        id: "server-2",
+        host: "127.0.0.1",
+        port: 3002,
+        healthy: false
+    },
+    {
+        id: "server-3",
+        host: "127.0.0.1",
+        port: 3003,
+        healthy: false
+    }
+];
+
+//health check
+function updateHealth(replica, healthy) {
+    if (replica.healthy !== healthy) {
+        replica.healthy = healthy;
+        console.log(`[HEALTH] ${replica.id} -> ${healthy ? 'UP' : 'DOWN'}`);
+    }
+}
+
+function checkHealth(replica) {
+    const request = http.get(
+        {
+            hostname: replica.host,
+            port: replica.port,
+            path: "/health",
+            timeout: HEALTH_CHECK_TIMEOUT
+        },
+        (response) => {
+            response.resume();
+            updateHealth(replica, response.statusCode === 200);
+        }
+    );
+
+    request.on("timeout", () => {
+        request.destroy();
+    });
+
+    request.on("error", () => {
+        updateHealth(replica, false);
+    });
+}
+
+function checkAllReplicas() {
+    for (const replica of serverReplicas) {
+        checkHealth(replica);
+    }
+}
+
+// initial health check
+checkAllReplicas();
+
+// continue health checks after every 5 seconds
+setInterval(
+    checkAllReplicas,
+    HEALTH_CHECK_INTERVAL
+);
+
+
+// Round Robin selection of servers
+let nextIndex = 0;
+
+function getNextHealthyReplica() {
+    for (let i = 0; i < serverReplicas.length; i++) {
+        const index = (nextIndex + i) % serverReplicas.length;
+
+        const replica = serverReplicas[index];
+
+        if (replica.healthy) {
+            nextIndex = (index + 1) % serverReplicas.length;
+            return replica;
+        }
+    }
+
+    return null;
+}
+
+
+// Load Balancer Server
+const server = http.createServer((req, res) => {
+
+    // load balancer own health end point
+    if (req.method === "GET" && req.url === "/lb/health") {
+
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(
+            JSON.stringify({
+                status: "ok",
+                serverReplicas: serverReplicas.map((replica) => ({
+                    id: replica.id,
+                    host: replica.host,
+                    port: replica.port,
+                    healthy: replica.healthy
+                }))
+            })
+        )
+        return;
+    }
+
+    // choose server instance
+    const replica = getNextHealthyReplica();
+
+    if (!replica) {
+        res.writeHead(503, {
+            "Content-Type": "application/json"
+        });
+
+        res.end(
+            JSON.stringify({
+                error: "No healthy backend replicas available"
+            })
+        );
+
+        return;
+    }
+
+    console.log(`[LB] ${req.method} ${req.url} -> ${replica.id}`);
+
+    // forward the request
+    const forwardedFor = req.headers["x-forwarded-for"]
+        ? `${req.headers["x-forwarded-for"]}, ${req.socket.remoteAddress}`
+        : req.socket.remoteAddress;
+
+    const proxyRequest = http.request(
+        {
+            hostname: replica.host,
+            port: replica.port,
+            path: req.url,
+            method: req.method,
+            headers: {
+                ...req.headers,
+                host: `${replica.host}:${replica.port}`,
+                "x-forwarded-for": forwardedFor,
+                "x-load-balancer": "mq-lb-rprl"
+            },
+            timeout: REQUEST_TIMEOUT
+        },
+        (proxyResponse) => {
+            res.writeHead(
+                proxyResponse.statusCode,
+                proxyResponse.headers
+            );
+            proxyResponse.pipe(res);
+        }
+    );
+
+    // backend failed
+    proxyRequest.on("error", (error) => {
+        console.error(
+            `[LB] ${replica.id} failed:`,
+            error.message
+        );
+
+        // change the server instance state and remove it from rotation
+        replica.healthy = false;
+
+        if (!res.headersSent) {
+            res.writeHead(502, { "Content-Type": "application/json" });
+
+            res.end(
+                JSON.stringify({
+                    error: "Backend server unavailable",
+                    backend: replica.id
+                })
+            );
+        } else {
+            res.destroy();
+        }
+    });
+
+    // backend timeout
+    proxyRequest.on("timeout", () => {
+        proxyRequest.destroy(
+            new Error("Backend request timeout")
+        );
+    });
+
+    req.pipe(proxyRequest);
+});
+
+
+server.listen(LB_PORT, () => {
+
+    console.log(
+        `Load balancer running on http://localhost:${LB_PORT}`
+    );
+
+    console.log(
+        "Backends:"
+    );
+
+    for (const replica of serverReplicas) {
+        console.log(
+            `  ${replica.id} -> http://${replica.host}:${replica.port}`
+        );
+    }
+});
